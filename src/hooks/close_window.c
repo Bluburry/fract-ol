77550d8e594f53072d8d195a9a4a76b9d43d96fb
Tiago <110920531+Bluburry@users.noreply.github.com>
2023-08-18 17:00:35 +0100

    fixing leaks

--- src/hooks/close_window.c
+++ src/hooks/close_window.c
@@ -14,18 +14,11 @@
 
 int	hook_close_window(t_controller *ctrl)
 {
-	if (!ctrl)
-		exit(0);
-	if (!ctrl->img)
-		mlx_destroy_image(ctrl->mlx, ctrl->img);
-	if (!ctrl->win)
-		mlx_destroy_window(ctrl->mlx, ctrl->win);
-	if (!ctrl->mlx)
-	{
-		mlx_loop_end(ctrl->mlx);
-		mlx_destroy_display(ctrl->mlx);
-		free(ctrl->mlx);
-	}
+	mlx_destroy_image(ctrl->mlx, ctrl->img);
+	mlx_destroy_window(ctrl->mlx, ctrl->win);
+	mlx_loop_end(ctrl->mlx);
+	mlx_destroy_display(ctrl->mlx);
+	free(ctrl->mlx);
 	free(ctrl);
 	exit(0);
 }
